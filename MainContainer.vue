a4ec8787d70e0d62760e32f69bf16c2f97b0a6a7
Mykhailo Klymchuk <80036865+andrewparhomchukswitch@users.noreply.github.com>
2021-03-05 19:22:38 +0200

    deploy new version

---
+++ MainContainer.vue
@@ -0,0 +1,32 @@
+<template>
+  <main class="main-container">
+    <slot />
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'MainContainer'
+}
+</script>
+
+<style lang="scss">
+.main-container {
+  position: relative;
+  height: 72%;
+  width: 42%;
+  min-width: 580px;
+  overflow: auto;
+
+  background: #FFFFFF;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  box-sizing: border-box;
+  box-shadow: 0px 4px 80px rgba(0, 0, 0, 0.09);
+  border-radius: 40px;
+
+  @media all and (max-width: 700px) {
+    min-width: unset;
+    width: 95%;
+  }
+}
+</style>
